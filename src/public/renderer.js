d0f343a3804e4afd4d87af3dad01cbb320bac9ef
pridi0t <yoeun18@gmail.com>
2024-10-06 03:54:56 +0900

    CSS 추가

--- src/public/renderer.js
+++ src/public/renderer.js
@@ -1,8 +1,29 @@
-const noteTitle = document.getElementById("noteTitle");
-const noteContent = document.getElementById("noteContent");
+const noteTitle = document.getElementById("inputNoteTitle");
+const noteContent = document.getElementById("inputNoteContent");
 const saveButton = document.getElementById("saveButton");
 const notesList = document.getElementById("notesList");
-const contentBox = document.getElementById("contentbox");
+const contentBox = document.getElementById("noteContent");
+
+function addListButton(id, title) {
+    // 버튼 생성
+    const newBtn = document.createElement("button");
+    newBtn.innerHTML = `${title}`;
+    newBtn.setAttribute("noteId", id);;
+    newBtn.addEventListener("click", () => loadNoteContent(id));
+
+    // 버튼 스타일 추가
+    newBtn.style.marginTop = "3px";
+    newBtn.style.padding = "10px";
+    newBtn.style.width = "100%";
+    newBtn.style.border = "1px solid black";
+    newBtn.style.borderRadius = "5px";
+    newBtn.style.textAlign = "left";
+    newBtn.style.overflow = "hidden";
+    newBtn.style.textOverflow = "ellipsis";
+    newBtn.style.backgroundColor = "white";
+
+    notesList.append(newBtn);
+}
 
 // 전체 노트 불러오기
 function loadTitleList() {
@@ -12,14 +33,7 @@ function loadTitleList() {
     window.api.loadTitleList().then((notes) => {
         notes.forEach((note) => {
             const {id, title} = note;
-            
-            // 버튼 생성
-            const newBtn = document.createElement("button");
-            newBtn.innerHTML = `${title}`;
-            newBtn.setAttribute("noteId", id);;
-            newBtn.addEventListener("click", () => loadNoteContent(id));
-
-            notesList.append(newBtn);
+            addListButton(id, title);
         });
     }).catch((err) => {
         console.error("[ERROR/renderer.js] - loadTitleList",err);
@@ -32,7 +46,7 @@ function loadNoteContent(id) {
     window.api.loadNoteContent(id).then((note) => {
         const { title, content } = note;
         contentBox.innerHTML = "";
-        contentBox.innerHTML = `<h2>${title}</h2><p>${content}</p>`;
+        contentBox.innerHTML = `<h2>${title}</h2><hr/><p>${content}</p>`;
     }).catch((err) => {
         console.error("[ERROR/renderer.js] - loadNoteContent", err);
         alert("노트를 불러오는데 실패했습니다.");
@@ -48,13 +62,7 @@ saveButton.addEventListener("click", () => {
     if (newNote.title) {
         window.api.saveNote(newNote).then((id) => {
             alert("노트 저장됨");
-            // 성공시 마지막 요소만 끝에 추가
-            const newBtn = document.createElement("button");
-            newBtn.innerHTML = `${newNote.title}`;
-            newBtn.setAttribute("noteId", id);
-            newBtn.addEventListener("click", () => loadNoteContent(id));
-            
-            notesList.append(newBtn);
+            addListButton(id, newNote.title);
         }).catch((err) => {
             console.error("[ERROR/index.html] - saveNote", err);
             alert("노트 저장 실패");
