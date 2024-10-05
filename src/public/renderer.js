d66274ce5ed15d150ccbb984206c61d3fba69a3f
pridi0t <yoeun18@gmail.com>
2024-10-06 02:08:57 +0900

    노트 저장시 전체를 로드하지 않고 마지막에만 추가하도록 수정

--- src/public/renderer.js
+++ src/public/renderer.js
@@ -3,17 +3,19 @@ const noteContent = document.getElementById("noteContent");
 const saveButton = document.getElementById("saveButton");
 const notesList = document.getElementById("notesList");
 
-// 노트 불러오기
+// 전체 노트 불러오기
 function loadNotes() {
+    // 이전 노트 초기화
+    notesList.innerHTML = "";
+    
     window.api.loadNotes().then((notes) => {
-        // notesList.innerHTML = notes.map((title, content) => `<div><h4>${title}</h4><p>${content}</p></div>`).join("");
         notes.forEach(({dataValues}) => {
             const {title, content} = dataValues;
             const newNote = document.createElement("div");
             newNote.innerHTML = `<h4>${title}</h4><p>${content}</p>`;
             notesList.append(newNote)
         });
-    }).catch((err) => { 
+    }).catch((err) => {
         console.error("[ERROR/index.html] - loadNotes",err);
         alert("노트를 불러오는데 실패했습니다");
     });
@@ -21,14 +23,17 @@ function loadNotes() {
 
 // 노트 저장
 saveButton.addEventListener("click", () => {
-    const note = {
+    const newNote = {
         title: noteTitle.value.trim(),
         content: noteContent.value
     }
-    if (note.title) {
-        window.api.saveNote(note).then(() => {
+    if (newNote.title) {
+        window.api.saveNote(newNote).then(() => {
             alert("노트 저장됨");
-            loadNotes();
+            // 성공시 마지막 요소만 끝에 추가
+            const newEle = document.createElement("div");
+            newEle.innerHTML = `<h4>${newNote.title}</h4><p>${newNote.content}</p>`;
+            notesList.append(newEle);
         }).catch((err) => {
             console.error("[ERROR/index.html] - saveNote", err);
             alert("노트 저장 실패");
